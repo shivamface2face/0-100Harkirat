9348f2ef6ecdb7690afc7afdbbd6a2d5910fb1db
Shivam Singh <shivamfaceuit@gmail.com>
2024-04-12 10:44:35 +0530

    number file has been added

---
+++ Week-1/week1.3/number.js
@@ -0,0 +1,19 @@
+function explainParseInt(value){
+    let result=parseInt(value);
+     console.log(result);
+}
+
+
+explainParseInt("42");
+explainParseInt("42px");
+explainParseInt("3.14");
+
+
+function explainParseFloat(value){
+   let result=parseFloat(value);
+     console.log(result);
+}
+
+explainParseFloat("3.14");
+explainParseFloat("42");
+explainParseFloat("42px");
