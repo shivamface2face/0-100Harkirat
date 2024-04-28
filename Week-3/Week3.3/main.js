2cca7d6fbcb56871e0237951422a211364ccf1c3
Shivam Singh <shivamfaceuit@gmail.com>
2024-04-28 12:18:22 +0530

    jwt and zod has been added

---
+++ Week-3/Week3.3/main.js
@@ -0,0 +1,52 @@
+const jwt = require("jsonwebtoken");
+const jwtPassword = "secret";
+const zod = require("zod");
+
+const emailSchima = zod.string().email();
+const passwordSchima = zod.string().min(6);
+
+function signJwt(username, password) {
+  const userResponse = emailSchima.safeParse(username);
+  const passResponse = passwordSchima.safeParse(password);
+
+  if (!userResponse.success || !passResponse.success) {
+    return null;
+  }
+
+  const signature = jwt.sign(
+    {
+      username,
+    },
+    jwtPassword,
+  );
+  return signature;
+}
+
+function verifyJwt(token) {
+     try{
+       jwt.verify(token,jwtPassword);
+       return true;
+     }catch(e){
+       
+     }
+    return false;
+  }
+  
+  
+  function decodeJwt(token) {
+   const decode=jwt.decode(token);
+    if(decode){
+     return true;
+    }else{
+      return false;
+    }
+  }
+
+
+let ans = signJwt("shivamsingh@email.com", "asdasdasdas");
+
+console.log(ans);
+let ans2=decodeJwt("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNoaXZhbXNpbmdoQGVtYWlsLmNvbSIsImlhdCI6MTcxNDI4NjY0MH0.sColYHgw7jnCNNKmlgnXZyjDAfih2gWiV-nncLA0bWQ");
+console.log(ans2);
+let ans3=verifyJwt("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNoaXZhbXNpbmdoQGVtYWlsLmNvbSIsImlhdCI6MTcxNDI4NjY0MH0.sColYHgw7jnCNNKmlgnXZyjDAfih2gWiV-nncLA0bWQ");
+console.log(ans3);
